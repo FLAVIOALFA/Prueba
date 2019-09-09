c1c516bf0168a338c974365b72557f2cd18af6eb
Flavio <fg.alfaro94@gmail.com>
2019-09-09 13:31:36 -0300

    base style ok

--- src/app/pages/ad/ad.module.ts
+++ src/app/pages/ad/ad.module.ts
@@ -15,6 +15,7 @@ import { SpotComponent } from './components/spot/spot.component';
 import { TagsComponent } from './components/tags/tags.component';
 import { TagComponent } from './components/tags/tag/tag.component';
 import { AdComponent } from './ad.component';
+import { ReproductionControlComponent } from './components/reproduction-control/reproduction-control.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 @NgModule({
@@ -23,7 +24,8 @@ import { CommonModule } from '@angular/common';
         SpotComponent,
         AdComponent,
         TagsComponent,
-        TagComponent
+        TagComponent,
+        ReproductionControlComponent
     ],
     imports: [
         CommonModule,
---
+++ src/app/pages/ad/components/reproduction-control/reproduction-control.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-reproduction-control',
+  templateUrl: './reproduction-control.component.html'
+})
+export class ReproductionControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
